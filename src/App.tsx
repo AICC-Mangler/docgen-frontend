4d1cb5e9f021a450455afad53ff006a888f7bb3a
Wasa-B <ghd1876@gmail.com>
2025-08-26 11:01:31 +0900

    .env jenkins test

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const env_test = import.meta.env.VITE_ENV_TEST;
   return (
     <>
       <div>
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React {env_test}</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
