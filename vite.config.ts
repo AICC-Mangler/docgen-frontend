89976f6b7f2165a29cf5da6d1e0ea7aafa315ea0
김준영 <1090jun@naver.com>
2025-08-27 13:40:11 +0900

    배포 수정

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(
+  {
   plugins: [react()],
   server: {
     port: 8181,
     proxy: {
       '/api': {
-        target: 'http://localhost:3100',
+        // target: 'http://localhost:3100',
+        target: 'http://192.168.10.220:3100',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
