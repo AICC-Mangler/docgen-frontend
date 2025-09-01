7858a12fe0d8df6debc35675c9fabf219d498f03
Wasa-B <ghd1876@gmail.com>
2025-09-01 15:45:20 +0900

    view 나눔

--- src/components/pages/document/requirementDocumentViewer.tsx
+++ src/components/pages/document/requirementDocumentViewer.tsx
@@ -6,98 +6,17 @@ import 'handsontable/dist/handsontable.full.css';
 import { api } from '../../../api';
 import { useParams } from 'react-router-dom';
 import { API_BASE_URL } from '../../../api/apiClient';
+import PRD_Viewer from './component/PRD_Viewer';
 
 const test_url = 'http://localhost:3100/document/requirement/file/68b007e231b920ebbad0910b'
 
-interface MergeCell {
-  row: number;
-  col: number;
-  rowspan: number;
-  colspan: number;
-}
-interface TableData {
-  data: (string | number | null)[][];
-  mergeCells: MergeCell[];
-  colHeader: (string)[];
-}
 
 const RequirementDocumentViewer: React.FC = () => {
-  const [tableData, setTableData] = useState<TableData>({
-    data: [],
-    mergeCells: [],
-    colHeader: [],
-  })
   const {document_id} = useParams();
-  const document_url = `/document/requirement/file/${document_id}`
-  useEffect(() => {
-    const fetchExcel = async () => {
-      const response = await api.get(document_url,{
-        responseType: 'arraybuffer'
-      })
-
-      const data = new Uint8Array(response.data);
-      const workbook = XLSX.read(data, { type: 'array' });
-      const sheetName = workbook.SheetNames[0];
-      const worksheet = workbook.Sheets[sheetName];
-
-      const jsonData : any = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
-
-      const merges = (worksheet['!merges'] || []).map((m) => {
-        return {
-          row: m.s.r - 1, // row 시작 좌표 보정
-          col: m.s.c,
-          rowspan: m.e.r - m.s.r + 1,
-          colspan: m.e.c - m.s.c + 1,
-        };
-      }).filter(m => m.row >= 0);
-      setTableData(
-        {
-          data:jsonData,
-          mergeCells:merges,
-          colHeader:jsonData[0]
-        }
-      )
-    };
-
-    fetchExcel();
-  }, []);
-
-  const download_btn = ()=>{
-    location.href = `${API_BASE_URL}${document_url}`
-  }
 
   return (
     <div>
-      <button className='bg-green-600 text-white shadow-md' onClick={download_btn}>download</button>
-      <div className='overflow-y-scroll h-[600px]'>
-      <HotTable
-        data={tableData.data.slice(1)}
-        colHeaders={tableData.colHeader}
-        rowHeaders={true}
-        width="100%"
-        height="auto"
-        licenseKey="non-commercial-and-evaluation"
-        mergeCells={tableData.mergeCells} // <- mergeCells prop에 전달
-        manualColumnResize={true}
-        manualRowResize={true}
-        readOnly={true} // 읽기 전용
-        dropdownMenu={['filter_by_condition', 'filter_action_bar', 'filter_by_value']}
-        filters={true}
-        rowHeights={40}
-        cells={(row, col) => {
-          const cellProperties: Handsontable.CellMeta = {};
-          if (col < 3) {
-            cellProperties.className = 'htCenter htMiddle font-bold';
-          }
-          if (col < 2){
-            cellProperties.width = 70;
-          }
-          
-          return cellProperties;
-        }}
-        className="htMiddle font-bold"
-      />
-      </div>
+      <PRD_Viewer document_id={document_id}/>
     </div>
   );
 };
